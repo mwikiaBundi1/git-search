42cfb83014b042c58efa0afd2be0c024d3a5b4f9
{mwikiaBundi1} <{mwikiam@gmail.com}>
2020-04-13 16:47:41 +0300

    add description in about us page

--- src/app/user-form/user-form.component.ts
+++ src/app/user-form/user-form.component.ts
@@ -10,6 +10,7 @@ export class UserFormComponent implements OnInit {
   newUser = new User("");
   @Output() addUser = new EventEmitter<User>();
 
+
   submitUser() {
     this.addUser.emit(this.newUser);
   }
