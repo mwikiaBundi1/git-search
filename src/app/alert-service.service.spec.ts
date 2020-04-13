60b935c5a57d6f5b857da722ca4faffecf25ed20
{mwikiaBundi1} <{mwikiam@gmail.com}>
2020-04-13 16:34:36 +0300

    add alert service

--- src/app/alert-service.service.spec.ts
+++ src/app/alert-service.service.spec.ts
@@ -4,8 +4,7 @@ import { AlertServiceService } from './alert-service.service';
 
 describe('AlertServiceService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
-
-  it('should be created', () => {
+    it('should be created', () => {
     const service: AlertServiceService = TestBed.get(AlertServiceService);
     expect(service).toBeTruthy();
   });
